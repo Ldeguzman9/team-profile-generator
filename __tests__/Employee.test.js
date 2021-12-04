e81667bc0ba2c8ec01e152aebc5a49c97cb7c0ef
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2021-12-04 13:43:25 -0800

    employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,49 @@
 const inquirer = require("inquirer");
 const Employee = require("../lib/Employee");
 
-test("create an Employee object", () => {
-  const employee = new Employee("Dave", 2020, "dave@fakeemail.com");
+test("Create employee object", () => {
+  const e = new Employee();
+  expect(typeof e).toBe("object");
+});
 
-  expect(typeof employee).toBe("object");
-  expect(employee.name).toEqual(expect.any(String));
-  expect(employee.id).toEqual(expect.any(Number));
-  expect(employee.email).toEqual(expect.any(String));
+test("Set  employee name via constructor arguments", () => {
+  const name = "Alice";
+  const employee = new Employee(name);
+  expect(employee.name).toBe(name);
+});
+
+test("Set employee id via constructor argument", () => {
+  const testValue = 100;
+  const employee = new Employee("Foo", testValue);
+  expect(employee.id).toBe(testValue);
+});
+
+test("Set employee email via constructor argument", () => {
+  const testValue = "test@test.com";
+  const employee = new Employee("Foo", 1, testValue);
+  expect(employee.email).toBe(testValue);
+});
+
+test("Can get employee name via getName()", () => {
+  const testValue = "Alice";
+  const employee = new Employee(testValue);
+  expect(employee.getName()).toBe(testValue);
+});
+
+test("Can get employee id via getId()", () => {
+  const testValue = 100;
+  const employee = new Employee("Foo", testValue);
+  expect(employee.getId()).toBe(testValue);
+});
+
+test("Can get employee email via getEmail()", () => {
+  const testValue = "test@test.com";
+  const employee = new Employee("Foo", 1, testValue);
+  expect(employee.getEmail()).toBe(testValue);
+});
+
+test('getRole() should return "Employee"', () => {
+  const testValue = "Employee";
+  const employee = new Employee("Alice", 1, "test@test.com");
+  expect(employee.getRole()).toBe(testValue);
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,11 +1,9 @@
 const inquirer = require("inquirer");
 const Engineer = require("../lib/Engineer");
 
-test("create an Engineer object", () => {
-  const engineer = new Engineer("Lauren", 2021, "lauren@fakeemail.com");
+test("create an Engineer object with github account", () => {
+  const github = "Ldeguzman9";
+  const engineer = new Engineer("Lauren", 2021, "lauren@fakeemail.com", github);
 
-  expect(typeof employee).toBe("object");
-  expect(engineer.name).toEqual(expect.any(String));
-  expect(engineer.id).toEqual(expect.any(Number));
-  expect(engineer.email).toEqual(expect.any(String));
+  expect(engineer.github).toBe(github);
 });
